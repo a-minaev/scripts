2415952d6a368b1fc3de850a36afa25b4e0c9b0b
Alexey <alexey.a.minaev@gmail.com>
2026-04-02 17:04:19 -0500

    added commands for checking resource usage

--- server-stats.sh
+++ server-stats.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# script to monitor server stats
 
+echo -e "\n Total CPU usage"
+top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage: " $2+$4 "%"}'
+
+echo -e "\n Total Memory usage"
+free -m | awk 'NR==2{print "Free Memory: " $2 "MB  Used Memory: " $3 "MB"}'
+
+echo -e "\n Total Disk usage"
+df -h --total | awk 'NR==9{print "Available: " $4 "  Used: " $3}'
+
+echo -e "\n Top 5 CPU Processes"
+ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 6
+
+echo -e "\n Top 5 Memory Processes"
+ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 6 
